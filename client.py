13e0d4ef3dceea61f254d655b2bf6f7a33582da2
downpour07 <choiheeyoung10@gmail.com>
2025-03-31 22:14:16 +0900

    Add WiFiStatusCode Func

--- client.py
+++ client.py
@@ -6,6 +6,18 @@ import main
 import serverinfo
 
 def WiFiStatusCode(status):
+    if status == network.STAT_IDLE:
+        return "WL_IDLE\n"
+    if status == network.STAT_CONNECTING:
+        return "WL_CONNECTING\n"
+    if status ==  network.STAT_WRONG_PASSWORD:
+        return "WL_WRONG_PASSWORD\n"
+    if status ==  network.STAT_NO_AP_FOUND:
+        return "WL_NO_AP_FOUND\n"
+    if status ==  network.STAT_CONNECT_FAILED:
+        return "WL_CONNECT_FAILED\n"
+    if status ==  network.STAT_GOT_IP:
+        return "WL_GOP_IP\n"
 
 def webSocketEvent(payload):
 
