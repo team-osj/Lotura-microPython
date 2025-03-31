0038368eabaf7916f5c92e9a03885f237eb22612
downpour07 <choiheeyoung10@gmail.com>
2025-03-31 22:11:18 +0900

    define func

---
+++ client.py
@@ -0,0 +1,16 @@
+import machine
+import network
+import ujson
+import mdns
+import main
+import serverinfo
+
+def WiFiStatusCode(status):
+
+def webSocketEvent(payload):
+
+def WiFiGotIP():
+
+def WiFiStationDisconnected():
+
+def NetworkInfo():
