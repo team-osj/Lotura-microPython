3802b515c1c98747945fddc868f4d2c82e8f7cdf
downpour07 <choiheeyoung10@gmail.com>
2025-03-31 22:24:55 +0900

    Add SendLog Func

--- send.py
+++ send.py
@@ -27,3 +27,22 @@ def SendStatus(ch, status):
         main.SERIAL.write("SendStatus Fail - No Server Connection\n")
 
 def SendLog(ch, log):
+    if ch == 1 and main.CH1Live == False:
+        return 1
+    if ch == 2 and main.CH2Live == False:
+        return 2
+    if main.WiFi.isconnected() and main.webSocket.isConnected() == True:
+        LogData = {
+            "title": "Log"
+        }
+        if ch == 1:
+            LogData["id"] = main.CH1DeviceNo
+        if ch == 2:
+            LogData["id"] = main.CH2DeviceNo
+        LogData["log"] = log
+        LogDataString = ujson.dumps(LogData)
+        main.webSocket.send(LogDataString)
+        return 0
+    else:
+        main.SERIAL.write("SendLog Fail - No Server Connection\n")
+        return 1
