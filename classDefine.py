86b82ba767e14b872d54cb9fb734b18855f235fb
downpour07 <choiheeyoung10@gmail.com>
2025-03-31 22:11:07 +0900

    define class

---
+++ classDefine.py
@@ -0,0 +1,20 @@
+import machine
+import ujson
+import math
+import time
+import base64
+from websocket import create_connection
+import send
+import main
+
+ADC_BITS = 12
+ADC_COUNTS = 1 << ADC_BITS
+
+SUPPLY_VOLTAGE = 3.3
+
+#class
+class Preferences:
+
+class EnergyMonitor:
+
+class WebSocketClient:
