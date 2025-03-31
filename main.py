a5c94bdee73873e85b42afc65a60c0744ff28f79
downpour07 <choiheeyoung10@gmail.com>
2025-03-31 22:23:12 +0900

    Add Main

--- main.py
+++ main.py
@@ -115,7 +115,95 @@ CH2SeCnt = 0
 
 wifiConnected = False
 
+SERIAL = machine.UART(0, baudrate=115200, tx=1, rx=3)
+preferences = classDefine.Preferences()
+
+ct1 = classDefine.EnergyMonitor()
+ct2 = classDefine.EnergyMonitor()
+
+PIN_FLOW1.irq(trigger=machine.Pin.IRQ_FALLING, handler=interrupt.Flow1)
+PIN_FLOW2.irq(trigger=machine.Pin.IRQ_FALLING, handler=interrupt.Flow2)
+
+doc = {}
+WiFi = 0
+
+webSocket = classDefine.WebSocketClient()
+
+# AsyncWebServer server(80) 
+
+timer = machine.Timer(0)
+
 #def
 def checkWifiStatus(timer):
+    global wifiConnected
+    if WiFi.isconnected():
+        if not wifiConnected:
+            wifiConnected = True
+            client.WiFiGotIP()
+    else:
+        if wifiConnected:
+            wifiConnected = False
+            client.WiFiStationDisconnected()
 
+#main
 if __name__ == "__main__":
+    WiFi = network.WLAN(network.STA_IF)
+    preferences.begin("config", True)
+    string = f"FW_VER : {serverinfo.BUILD_DATE}\n"
+    SERIAL.write(string)
+
+    ct1.current(PIN_CT1, 30.7)
+    ct2.current(PIN_CT2, 30.7)
+
+    value.SetDefaultVal()
+
+    modeDebug = PIN_DEBUG.value()
+
+    CH1Mode = PIN_CH1_MODE.value()
+    CH2Mode = PIN_CH2_MODE.value()
+
+    if modeDebug == 0:
+        SERIAL.write("YOU ARE IN THE DEBUG MODE !!!\n")
+    
+    string = f"CH1_Mode : {CH1Mode}\nCH2_Mode : {CH2Mode}\n"
+    SERIAL.write(string)
+
+    WiFi.disconnect()
+
+    for i in range(0, 30):
+        ct1.calclrms(1480)
+        ct2.calclrms(1480)
+    
+    string = f"Boot Heap : {gc.mem_free()}\n"
+    SERIAL.write(string)
+
+    PIN_STATUS.value(1)
+    
+    timer.init(period=1000, mode=machine.Timer.PERIODIC, callback=checkWifiStatus)
+
+    if apSsid == "":
+        SERIAL.write("Skip WiFi Setting Due to No SSID\n")
+    else:
+        string = f"Connecting to WiFi .. {apSsid}\n"
+        SERIAL.write(string)
+
+        if apPassword == "":
+            WiFi.connect(apSsid) 
+        else:
+            WiFi.connect(apSsid, apPassword)
+
+        wifiTimeOut = 0
+
+        while WiFi.isconnected():
+            SERIAL.write(".")
+            PIN_STATUS.value(0)
+            time.sleep(0.1)
+            PIN_STATUS.value(1)
+            time.sleep(0.1)
+            wifiTimeOut += 1
+
+            if wifiTimeOut > 25:
+                SERIAL.write("Skip WiFi Connection Due to Timeout")
+                break
+    
+    loop.loop()
