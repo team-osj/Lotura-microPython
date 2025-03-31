aa5bc9f0339c305883d4417840338e8e21211b4f
downpour07 <choiheeyoung10@gmail.com>
2025-03-31 22:11:28 +0900

    define func

---
+++ judment.py
@@ -0,0 +1,9 @@
+import machine
+import ujson
+import time
+import main
+import send
+
+def DryerStatusJudgment(ampsTRMS, cnt, m, prevMillisEnd, ChannelNum):
+
+def StatusJudgment(ampsTRMS, waterSensorData, LHour, cnt, m, prevMillisEnd, ChannelNum):
