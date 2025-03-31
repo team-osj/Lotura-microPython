a8391a83b4c468bef536cb2e7608726a2ba22fe1
downpour07 <choiheeyoung10@gmail.com>
2025-03-31 22:11:43 +0900

    define func

---
+++ ota.py
@@ -0,0 +1,16 @@
+import machine
+import esp
+import gc
+import time
+import uasyncio as asyncio
+from microdot import Microdot, Response
+import main
+import serverinfo
+import value
+import html
+
+def processor(var):
+
+def convertFileSize(bytes):
+
+class WebServer:
